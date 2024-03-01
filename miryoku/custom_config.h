dec7de4559cd0df125b481ca957366910a779356
ikos <ikos@realnetworks.com>
2024-03-01 12:25:44 +0100

    qwerty custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_QWERTY
+#define MIRYOKU_NAV_VI
+#define MIRYOKU_ALTERNATIVES_MOUSE_VI
+#define MIRYOKU_ALTERNATIVES_MEDIA_VI
